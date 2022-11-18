d9de7b0d45b5a4f3680253de80190f28f8b45b20
Lucas Barbieri <bertani.lucas@gmail.com>
2022-11-17 23:05:47 -0300

    setup front-end

---
+++ web/pages/index.tsx
@@ -0,0 +1,18 @@
+export default function Home() {
+  
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export const getServerSideProps =async () => {
+  const response = await fetch('http://localhost:3333/pools/count')
+  const data = await response.json()
+
+  console.log(data)
+  return {
+    props: {}
+  }
+}
